ab6e8cd8a69468cbeb741a825358ca360f9a3d78
chililil93 <44651008+chililil93@users.noreply.github.com>
2024-01-18 16:53:39 +0000

    Deploying to gh-pages from @ chililil93/Seamly2D@f35045ac28dbdd5eee931f238019bcc0a5517754 🚀

--- docs/search/classes_11.js
+++ docs/search/classes_11.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['undoevent_9680',['UndoEvent',['../d0/d5b/classUndoEvent.html',1,'']]],
-  ['uniondialog_9681',['UnionDialog',['../d0/dc3/classUnionDialog.html',1,'']]],
-  ['uniontool_9682',['UnionTool',['../d4/dd5/classUnionTool.html',1,'']]],
-  ['uniontoolinitdata_9683',['UnionToolInitData',['../d9/d2d/structUnionToolInitData.html',1,'']]],
-  ['userchangeevent_9684',['UserChangeEvent',['../db/da6/classVPE_1_1UserChangeEvent.html',1,'VPE']]]
+  ['undoevent_9727',['UndoEvent',['../d0/d5b/classUndoEvent.html',1,'']]],
+  ['uniondialog_9728',['UnionDialog',['../d0/dc3/classUnionDialog.html',1,'']]],
+  ['uniontool_9729',['UnionTool',['../d4/dd5/classUnionTool.html',1,'']]],
+  ['uniontoolinitdata_9730',['UnionToolInitData',['../d9/d2d/structUnionToolInitData.html',1,'']]],
+  ['userchangeevent_9731',['UserChangeEvent',['../db/da6/classVPE_1_1UserChangeEvent.html',1,'VPE']]]
 ];
--- docs/search/classes_b.js
+++ docs/search/classes_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['nodeinfo_9504',['NodeInfo',['../d7/d55/structNodeInfo.html',1,'']]],
-  ['nonscalingfillpathitem_9505',['NonScalingFillPathItem',['../df/d63/classNonScalingFillPathItem.html',1,'']]],
-  ['notchdata_9506',['NotchData',['../d2/daa/structNotchData.html',1,'']]]
+  ['newmeasurementsdialog_9549',['NewMeasurementsDialog',['../dc/da3/classNewMeasurementsDialog.html',1,'']]],
+  ['nodeinfo_9550',['NodeInfo',['../d7/d55/structNodeInfo.html',1,'']]],
+  ['nonscalingfillpathitem_9551',['NonScalingFillPathItem',['../df/d63/classNonScalingFillPathItem.html',1,'']]],
+  ['notchdata_9552',['NotchData',['../d2/daa/structNotchData.html',1,'']]]
 ];
